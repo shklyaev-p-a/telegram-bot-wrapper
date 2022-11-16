ef8079ecbf46b3a1d16d943c3ed2f09868e7e994
unknown <gmittchel@mail.ru>
2022-11-16 13:21:36 +0300

    fix

--- src/Chaining/Links/Middleware.php
+++ src/Chaining/Links/Middleware.php
@@ -4,7 +4,10 @@ namespace BotWrapper\Chaining\Links;
 
 use BotWrapper\Bot;
 use BotWrapper\Chaining\Interfaces\BotInterface;
+use BotWrapper\Factories\MatcherFactory;
+use TelegramBot\Api\BaseType;
 use TelegramBot\Api\Client;
+use TelegramBot\Api\Types\CallbackQuery;
 use TelegramBot\Api\Types\Message;
 use TelegramBot\Api\Types\Update;
 use TelegramBot\Api\BotApi;
@@ -31,12 +34,28 @@ class Middleware implements BotInterface
             /** @var Message $message */
             $message = $update->getMessage();
 
-            foreach ($this->middlewares as $middleware) {
-                $middleware = new $middleware();
-                $middleware->make($bot, $message);
+            if ($message) {
+                $this->middlewaresLoop($bot, $message);
+            } else {
+                $this->callbackRegister($bot);
             }
         }, function () {
             return true;
         });
+    }
+
+    private function callbackRegister(Client $bot)
+    {
+        $bot->callbackQuery(function (CallbackQuery $message) use (/* @var BotApi $bot */ $bot) {
+            $this->middlewaresLoop($bot, $message);
+        });
+    }
+
+    private function middlewaresLoop(Client $bot, BaseType $message)
+    {
+        foreach ($this->middlewares as $middleware) {
+            $middleware = new $middleware();
+            $middleware->make($bot, $message);
+        }
     }
 }
--- src/Chaining/Middleware.php
+++ src/Chaining/Middleware.php
@@ -2,10 +2,10 @@
 
 namespace BotWrapper\Chaining;
 
+use TelegramBot\Api\BaseType;
 use TelegramBot\Api\Client;
-use TelegramBot\Api\Types\Message;
 
 abstract class Middleware
 {
-    abstract function make(Client $bot, Message $message = null);
+    abstract function make(Client $bot, BaseType $message = null);
 }
